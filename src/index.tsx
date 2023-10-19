148e2b862bb12a7cbfd1097da2bec9a5abac2fa3
22 <60903333+nini22P@users.noreply.github.com>
2023-10-19 19:40:25 +0800

    Move App

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'normalize.css'
-
-const App = () => {
-  return <div>Hello, world!</div>
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
