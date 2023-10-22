db0e47c49b3dbb09e78e9bb7c2368dfb0e779ab1
22 <60903333+nini22P@users.noreply.github.com>
2023-10-22 19:45:34 +0800

    Add StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './globals.css'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
